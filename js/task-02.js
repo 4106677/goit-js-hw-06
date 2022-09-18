63d76c465b10cb36d6b169bc2bde2566c90bff68
Yaroslav Oliinyk <4106677@gmail.com>
2022-09-18 15:45:40 +0300

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,9 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
 
+const list = document.querySelector("#ingredients");
+const listEl = [];
+
+ingredients.forEach((element) => {
+  const li = document.createElement("li");
+  li.classList.add("item");
+  li.textContent = element;
+  listEl.push(li);
+});
+
+list.append(...listEl);
