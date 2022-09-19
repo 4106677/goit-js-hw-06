50315c5c3259a4173af6162978ab918d0122de9e
Yaroslav Oliinyk <4106677@gmail.com>
2022-09-19 13:14:00 +0300

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector(".change-color");
+const text = document.querySelector(".color");
+const body = document.querySelector("body");
+
+button.addEventListener("click", onButtonClick);
+
+function onButtonClick() {
+  body.style.backgroundColor = getRandomHexColor();
+  // console.log(text.textContent);
+  text.textContent = getRandomHexColor();
+}
