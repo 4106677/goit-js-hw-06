b2dd84ae15b6d2f2f715c4c55fe20e1134f842a7
Yaroslav Oliinyk <4106677@gmail.com>
2022-09-19 12:27:05 +0300

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onLoginBtn);
+
+function onLoginBtn(event) {
+  event.preventDefault();
+
+  //   const formData = new FormData(event.currentTarget);
+
+  //   formData.forEach((value, name) => {
+  //     if (value === "") {
+  //       alert("Заповни всі поля, друже!");
+  //     }
+
+  //     console.log(name, value);
+
+  //     form.reset();
+  //   });
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Заповни всі поля, друже!");
+  }
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
